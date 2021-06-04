86c15d5f400cdcdcc1b2b53b26d66bdd658b7d46
Dmytro Antonov <okpanda11@gmail.com>
2021-06-04 14:17:11 +0300

    fixed sql script

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `cars_rent` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `cars_rent` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 */;
 USE `cars_rent`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
